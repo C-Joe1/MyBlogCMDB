3c86f980ddfd82ece220aa6234a5ae1d6eaf5c06
C_Joe <cjoe.dev.cn@gmail.com>
2023-03-21 01:26:39 +0800

    Update left_menu label & README

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,10 +2,10 @@
 import {h, onMounted} from "vue";
 import {RouterLink, RouterView} from 'vue-router'
 import {NIcon} from "naive-ui";
-import {AdminPanelSettingsFilled} from "@vicons/material";
-import {Person48Filled, People32Filled, ContentView32Filled} from "@vicons/fluent";
+import {Person48Filled, People32Filled, ContentView32Filled, TextBulletListSquare24Filled, CalendarSettings20Filled} from "@vicons/fluent";
 import {DashboardFilled} from "@vicons/antd";
 import {UserCog} from "@vicons/fa";
+import {Server} from "@vicons/ionicons5";
 
 function renderIcon(icon) {
   return () => h(NIcon, null, {default: () => h(icon)});
@@ -27,18 +27,45 @@ const menuOptions = [
   {
     label: "权限控制",
     key: "permission-control",
-    icon: renderIcon(AdminPanelSettingsFilled),
+    icon: renderIcon(CalendarSettings20Filled),
     children: [
       {
-        label: "成员管理",
+        label: "成员组",
+        key: "group-management",
+        icon: renderIcon(People32Filled),
+      },
+      {
+        label: "成员",
         key: "member-management",
         icon: renderIcon(Person48Filled),
       },
+    ]
+  },
+  {
+    label: "业务概念",
+    key: "business-concept",
+    icon: renderIcon(TextBulletListSquare24Filled),
+    children: [
       {
-        label: "组管理",
-        key: "group-management",
-        icon: renderIcon(People32Filled),
-      }
+        label: "机器",
+        key: "machine",
+        icon: renderIcon(Server),
+      },
+      {
+        label: "群组",
+        key: "group",
+        icon: renderIcon(),
+      },
+      {
+        label: "安全组",
+        key: "safely-group",
+        icon: renderIcon(),
+      },
+      {
+        label: "资源池",
+        key: "resource-pool",
+        icon: renderIcon(),
+      },
     ]
   }
 ];
